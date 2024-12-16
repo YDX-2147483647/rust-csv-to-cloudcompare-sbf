a3044ba1f1f9d6b5aa8c980ffa300a3980ad5edb
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2024-12-16 15:24:32 +0800

    feat: CLI

--- README.md
+++ README.md
@@ -3,12 +3,13 @@
 Convert a point cloud from CSV to [SBF (simple binary file)][sbf] format.
 
 ```shell
-$ cat ./data/example.csv | cargo run
+$ cat ./data/example.csv | cargo r -- -o ./out/example.sbf
+Write to "./out/example.sbf" (text metadata) and "./out/example.sbf.data" (binary data).
 ```
 
 ## Data format
 
-Input CSV: `(x, y, z, Re, Im)` array saved by NumPy `savetxt`.
+Input CSV: `(x, y, z, Re, Im)` array saved by NumPy `savetxt`.（因有 UTF-8 / UTF-16 编码问题，使用 stdin 而非 args。）
 
 Output SBF: See [CloudCompare doc][sbf].
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::path::PathBuf;
 use std::{error::Error, io, process};
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(long, short)]
+    output: PathBuf,
+}
 
 #[derive(Debug, serde::Deserialize)]
 struct Point {
@@ -9,7 +19,8 @@ struct Point {
     imag: f64,
 }
 
-fn example() -> Result<(), Box<dyn Error>> {
+/// Convert stdin (CSV) to output (SB)
+fn convert(_meta: &PathBuf, _data: &PathBuf) -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(false)
         .from_reader(io::stdin());
@@ -21,7 +32,18 @@ fn example() -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    if let Err(err) = example() {
+    let cli = Cli::parse();
+
+    // Validate
+    assert_eq!(cli.output.extension().unwrap(), "sbf");
+    let data_output = cli.output.with_extension("sbf.data");
+    println!(
+        "Write to {:?} (text metadata) and {:?} (binary data).",
+        cli.output, data_output
+    );
+
+    // Execute
+    if let Err(err) = convert(&cli.output, &data_output) {
         println!("error running example: {}", err);
         process::exit(1);
     }
